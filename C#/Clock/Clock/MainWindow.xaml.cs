2e859a08a814feabb3fadb064793f52b9dbae63d
Pawelllaz <pawelllaz@gmail.com>
2019-07-15 13:58:38 +0200

    WPF clock project

--- C#/Clock/Clock/MainWindow.xaml.cs
+++ C#/Clock/Clock/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,51 @@ namespace Clock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool flag = true;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            line1.Angle = 180;
+
+            Thread thread = new Thread(new ThreadStart(worker));
+            thread.Start();
+
+
+        }
+
+        private double setAngle()
+        {
+            double angle = 0;
+
+            angle = DateTime.Now.Second*6;
+            angle += (double)DateTime.Now.Millisecond/175;
+            //Console.WriteLine(angle);
+            return angle;
+        }
+
+        private void worker()
+        {
+            while(flag)
+            {
+                Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Render, (Action)delegate {
+                    line1.Angle = setAngle();
+                    Thread.Sleep(30);
+                });
+
+
+                /*Dispatcher.Invoke(() =>
+                {
+                    line1.Angle = DateTime.Now.Second;
+                });*/
+
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            flag = false;
         }
     }
 }
