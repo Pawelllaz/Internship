70b1d06d8ba90ded406174aa929ec47c6b4b0288
Pawelllaz <pawelllaz@gmail.com>
2019-07-16 13:51:05 +0200

    clock works on

--- C#/Clock/Clock/MainWindow.xaml.cs
+++ C#/Clock/Clock/MainWindow.xaml.cs
@@ -13,48 +13,79 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace Clock
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window//, INotifyPropertyChanged
     {
         private bool flag = true;
+        private double currentMin = 0;
+        private double currentH = 0;
+        
+        /*private string _txt;
+        public string Txt
+        {
+            get => _txt;
+            set
+            {
+                _txt = value;
+                OnPropertyChanged(nameof(Txt));
+            }
+        }*/
 
         public MainWindow()
         {
             InitializeComponent();
-
-            line1.Angle = 180;
+            DataContext = this;
 
             Thread thread = new Thread(new ThreadStart(worker));
             thread.Start();
 
-
         }
 
-        private double setAngle()
+        private double setSecAngle()
         {
             double angle = 0;
-
             angle = DateTime.Now.Second*6;
             angle += (double)DateTime.Now.Millisecond/175;
-            //Console.WriteLine(angle);
             return angle;
         }
 
         private void worker()
         {
-            while(flag)
+            Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Render, (Action)delegate
             {
-                Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Render, (Action)delegate {
-                    line1.Angle = setAngle();
-                    Thread.Sleep(30);
+                UpdateClock();
+            });
+
+            while (flag)
+            {
+                Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Render, (Action)delegate 
+                {
+                    secLine.Angle = setSecAngle();
+
+                    if (DateTime.Now.Minute != currentMin)
+                    {
+                        UpdateClock();
+                        if (currentH != DateTime.Now.Hour)
+                        {
+                            currentH = DateTime.Now.Hour;
+                            hLine.Angle = currentH * 30;
+                        }
+                    }
+                    //Txt = $"{Txt}a";
                 });
 
-
+                Thread.Sleep(30);
                 /*Dispatcher.Invoke(() =>
                 {
                     line1.Angle = DateTime.Now.Second;
@@ -63,9 +94,33 @@ namespace Clock
             }
         }
 
+        private void UpdateClock()
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+
+            animation.From = currentMin * 6;
+            currentMin = DateTime.Now.Minute;
+            animation.To = currentMin * 6 + 6;
+            //animation.AutoReverse = true;
+            animation.Duration = new Duration(TimeSpan.FromSeconds(0.3));
+            //animation.AccelerationRatio = 0.3;
+            RotateTransform rotateTransform = new RotateTransform();
+            rotateTransform.CenterX = 0;
+            rotateTransform.CenterY = 175;
+            minuteLine.RenderTransform = rotateTransform;
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, animation);
+        }
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             flag = false;
         }
+
+       /* public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }*/
     }
 }
