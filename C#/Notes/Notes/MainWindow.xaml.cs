4143d0a4fd4d088fbbf74c2813a1cd6b38a7338e
Pawelllaz <pawelllaz@gmail.com>
2019-07-24 13:54:37 +0200

    notes project works on

--- C#/Notes/Notes/MainWindow.xaml.cs
+++ C#/Notes/Notes/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -23,6 +25,22 @@ namespace Notes
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private bool flag = true;
+        private List<string> FilesPath = new List<string>();
+        private string directoryPath = @"C:\notes";
+        private string _newNoteTitle;
+        public string NewNoteTitle
+        {
+            get
+            {
+                return _newNoteTitle;
+            }
+            set
+            {
+                _newNoteTitle = value;
+                OnPropertyChanged(nameof(NewNoteTitle));
+            }
+        }
         private string _newNoteTxt;
         public string NewNoteTxt
         {
@@ -36,24 +54,89 @@ namespace Notes
                 OnPropertyChanged(nameof(NewNoteTxt));
             }
         }
+        private string _readedText;
+        public string ReadedText
+        {
+            get
+            {
+                return _readedText;
+            }
+            set
+            {
+                _readedText = value;
+                OnPropertyChanged(nameof(ReadedText));
+            }
+        }
+
 
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+            NewNoteTitle = "Add note title...";
+            NewNoteTxt = "Add note text...";
 
-            NewNoteTxt = "Add note...";
+            Thread thread = new Thread(new ThreadStart(Worker));
+            thread.Start();
 
-            
+            if (!CreateDirectory())
+            {
+                Application.Current.Shutdown();
+            }
+
 
         }
-        
-        private void txtName_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+
+        private void WriteNote(string path, string textToWrite)
         {
-            NewNoteTxt = string.Empty;
+            try
+            {
+                if (Directory.Exists(directoryPath))
+                {
+                    File.WriteAllText(path, textToWrite);
+                }
+                else
+                {
+                    Console.WriteLine("Directory does't exists");
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Saving file error: {0}", e.ToString());
+            }
+            
+        }
+        
+        private bool CreateDirectory()
+        {
+            try
+            {
+                if (!Directory.Exists(directoryPath))
+                {
+                    DirectoryInfo dir = Directory.CreateDirectory(directoryPath);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Creating directory fail: {0}", e.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        private void TitleName_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            EnableButtons();
+            NewNoteTitle = string.Empty;
             var resourceH = myWindow.Resources["TopPanelAnimation"] as Storyboard;
             resourceH?.Begin();
+        }
+
+        private void TxtName_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            NewNoteTxt = string.Empty;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -63,10 +146,106 @@ namespace Notes
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        private string MakeFilePath()
+        {
+            string filePath = directoryPath;
+            filePath += "\\" + NewNoteTitle + ".txt";
+            return filePath;
+        }
+
+        private void EnableButtons()
+        {
+            addButton.IsEnabled = true;
+            clearButton.IsEnabled = true;
+        }
+        private void DisableButtons()
+        {
+            addButton.IsEnabled = false;
+            clearButton.IsEnabled = false;
+        }
         private void AddButtonClick(object sender, RoutedEventArgs e)
         {
+            DisableButtons();
+            string filePath = null;
+            if (!String.IsNullOrEmpty(NewNoteTitle))
+            {
+                filePath = MakeFilePath();
+                WriteNote(filePath, NewNoteTxt);
+            }
             var resourceH = myWindow.Resources["TopPanelAnimationReverse"] as Storyboard;
             resourceH?.Begin();
+
+            NewNoteTxt = "Add note text...";
+            NewNoteTitle = "Add note title...";
+            
+        }
+
+        private void ClearButtonClick(object sender, RoutedEventArgs e)
+        {
+            DisableButtons();
+            var resourceH = myWindow.Resources["TopPanelAnimationReverse"] as Storyboard;
+            resourceH?.Begin();
+
+            NewNoteTxt = "Add note text...";
+            NewNoteTitle = "Add note title...";
+        }
+
+
+        // creating note
+        private void CreateNewNote(string path)
+        {
+            string textReaded = null;
+            try
+            {
+                textReaded = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Reading file error: {0}", e.ToString());
+            }
+            
+            Grid grid = new Grid();
+            grid.Width = 200;
+            grid.Height = 300;
+            grid.Background = Brushes.Yellow;
+            grid.Margin = new Thickness(5);
+
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = textReaded;
+
+            grid.Children.Add(textBlock);
+            myWrapPanel.Children.Add(grid);
+        }
+
+        private void Worker()
+        {
+            while (flag)
+            {
+
+                Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Render, (Action)delegate
+                {
+                    string[] files = null;
+                    if (Directory.Exists(directoryPath))
+                    {
+                        files = Directory.GetFiles(directoryPath);
+                    }
+                    foreach (string fileName in files)
+                    {
+                        if (!FilesPath.Contains(fileName))
+                        {
+                            FilesPath.Add(fileName);
+                            CreateNewNote(fileName);
+                        }
+                    }
+                
+                });
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            flag = false;
         }
     }
 }
